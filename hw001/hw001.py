c13bfbbedc4204929ddf3c102263e03f883eefe1
evbud <63953970+evbud@users.noreply.github.com>
2022-10-14 18:13:58 +0300

    Update hw001.py
    deleted space

--- hw001/hw001.py
+++ hw001/hw001.py
@@ -19,7 +19,7 @@ else:
 
 print('X | Y | Z | ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z')
 for x in [0, 1]:
-    for y in [0, 1 ]:
+    for y in [0, 1]:
         for z in [0, 1]:
             left = not (x or y or z)
             right = not x and not y and not z
